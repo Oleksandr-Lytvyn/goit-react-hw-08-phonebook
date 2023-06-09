fe96071856a9386b4a411ecb2786d0af5893b394
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-06-09 16:22:50 +0300

    set modal window

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,17 +1,21 @@
 import ReactDOM from 'react-dom';
+import { Backdrop, BackdropShadow, Window } from './Modal.styled';
 
 export const Modal = ({ children, setIsModal }) => {
   return ReactDOM.createPortal(
     <>
-      <div>modal window{children}</div>
-      <button
-        type="button"
-        onClick={() => {
-          setIsModal(false);
-        }}
-      >
-        X
-      </button>
+      <Backdrop>
+        <Window>modal window{children}</Window>
+        <button
+          type="button"
+          onClick={() => {
+            setIsModal(false);
+          }}
+        >
+          X
+        </button>
+      </Backdrop>
+      <BackdropShadow />
     </>,
     document.querySelector('#root-modal')
   );
